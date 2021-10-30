90f15517b7314292165a266821503e838e39a71e
YAPONE4 <lxprin@gmail.com>
2021-10-30 09:39:06 +0300

    Pr5

--- Practice5/src/ru/mirea/Main.java
+++ Practice5/src/ru/mirea/Main.java
@@ -1,5 +1,7 @@
 package ru.mirea;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void chisla(int n) {
@@ -18,13 +20,27 @@ public class Main {
             return n;
     }
 
-    public static int turnDigits(int n) {
-        if (n > 9)
-            return (n % 10 * (n / 10))
+    public static int turnDigits(int pr, int ch) {
+        Scanner rd = new Scanner(System.in);
+        int newone = rd.nextInt();
+        if (pr == 0 && newone == 0)
+            return (0);
+        else
+            if (pr == ch)
+                return(1 + turnDigits(newone, ch));
+            else
+                return(turnDigits(newone, ch));
     }
 
     public static void main(String[] args) {
+        System.out.println("Вывод последовательности от 1 до 5:");
 	    chisla(5);
+        System.out.println("Сумма цифр числа 345: ");
         System.out.println("\n" + sumDigits(345));
+        Scanner rd = new Scanner(System.in);
+        System.out.println("Введите последовательность, которая оканчивается двумя нулями (сколько" +
+                " повторяется число 3): ");
+        int newone = rd.nextInt();
+        System.out.println(turnDigits(newone, 3));
     }
 }
